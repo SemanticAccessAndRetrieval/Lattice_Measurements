edc56c027d7cd7e57e8506038fa5c6eb30b8f316
mountanton <mountanton@csd.uoc.gr>
2020-05-25 04:12:27 +0300

    Update Lattices_Algorithms.java

--- src/gr/forth/ics/isl/lattice/Lattices_Algorithms.java
+++ src/gr/forth/ics/isl/lattice/Lattices_Algorithms.java
@@ -1,7 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+This code belongs to the Semantic Access and Retrieval (SAR) group of the 
+Information Systems Laboratory (ISL) of the 
+Institute of Computer Science (ICS) of the  
+Foundation for Research and Technology – Hellas (FORTH)
+
+Nobody is allowed to use, copy, distribute, or modify this work.
+It is published for reasons of research results reproducibility.
+
+© 2020, Semantic Access and Retrieval group, All rights reserved
  */
 package gr.forth.ics.isl.lattice;
 
